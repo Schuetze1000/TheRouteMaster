7f59fb695b88a4f4956d272a881e8c96c2afb8bf
Schuetze1000 <87863203+Schuetze1000@users.noreply.github.com>
2023-04-28 14:30:05 +0200

    ---

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-center text-green-700">
-      Simple React Typescript Tailwind Sample
-    </h1>
+    <div className="w-full h-screen flex flex-cols text-center">
+        <div className="w-1/8 bg-slate-200 h-full p-4 hidden md:flex flex-col">
+          <div className="px-4">
+          
+          </div>
+          <div className="justify-stretch py-5 grid grid-cols-1 gap-3 my-auto">
+            <div className="bg-slate-100 shadow-md rounded-md px-4 py-3">
+              Tutorials
+            </div>
+            <div className="bg-slate-100 shadow-md rounded-md px-4 py-3">
+              Components
+            </div>
+            <div className="bg-slate-100 shadow-md rounded-md px-4 py-3">
+              Contact
+            </div>
+          </div>
+          <div>
+            FOOTER
+          </div>
+        </div>
+        <div className="grid grid-cols-1 gap-3 w-1/2 mx-auto h-1/2">
+          <div className="text-4xl pt-10">HELLO WORLD</div>
+          <div className="block">
+          <button className="bg-slate-100 shadow-md px-4 py-3"> Get started</button></div>
+        </div>
+     </div>
   );  
 }
 
