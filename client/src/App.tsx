cfb857e6eb9891584e630a3621a53c1810a65af7
Leonidas-maker <83175153+Leonidas-maker@users.noreply.github.com>
2023-05-03 21:07:37 +0200

    new hook file added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Login from './pages/login/login';
+import LinkButton from './components/LinkButton'
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
+      <LinkButton to="/login">Go to Login</LinkButton>
       <Routes>
         <Route path="/">
           <Route path="/login" element={<Login />} />
