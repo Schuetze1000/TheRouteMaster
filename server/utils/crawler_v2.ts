03031ab91c956b4ae7f87349148cb7668f48ee0e
Schuetze1000 <87863203+Schuetze1000@users.noreply.github.com>
2023-05-10 19:21:35 +0200

    -

--- server/utils/crawler_v2.ts
+++ server/utils/crawler_v2.ts
@@ -1,42 +1,56 @@
 import * as cheerio from "cheerio";
 import axios from "axios";
-import * as fs from 'fs';
+import * as fs from "fs";
+import crypto from "crypto";
 
-async function getICS() {
-  const name_list: string[] = [];
-  const uid_list: string[] = [];
+export async function getUIDS(): Promise<[string[],string[]]> {
   try {
-    const response = await axios.get(
-      "https://vorlesungsplan.dhbw-mannheim.de/ical.php"
-    );
-   
+    const name_list: string[] = [];
+    const uid_list: string[] = [];
+
+    const response = await axios.get("https://vorlesungsplan.dhbw-mannheim.de/ical.php");
+
     const data_html = cheerio.load(response.data);
     const elements = data_html('select[name="Kurse"] option');
 
-    for (let y = 0; y < elements.length; y++) {
+    for (let y = 1; y < elements.length; y++) {
       name_list.push(elements.get(y).attributes[0].value);
       uid_list.push(elements.get(y).attributes[1].value);
     }
-
-    for (let x = 0; x < name_list.length; x++) {
-      console.log(`Name= ${name_list[x]}; UID= ${uid_list[x]}`);
-    }
-    
-    for (let x = 1; x < name_list.length; x++) {
-      const response2 = await axios.get(`http://vorlesungsplan.dhbw-mannheim.de/ical.php?uid=${uid_list[x]}`);
-      fs.writeFile(`./ics/${uid_list[x]}.ics`, response2.data, err => {
-        if (err) {
-          console.error(err);
-        }
-        else {
-          console.log(`Downloaded ICS Name= ${name_list[x]}; UID= ${uid_list[x]}`)
-        }
-      });
-    }
+    return [name_list, uid_list];
 
   } catch (error) {
     console.error(error);
+    return undefined
   }
 }
 
-getICS()
+export async function getICS(name_list: string[] = [], uid_list: string[] = []): Promise<string[]>{
+  const hash_list: string[] = [];
+  for (let x = 0; x < name_list.length; x++) {
+    const path = `./ics/${uid_list[x]}.ics`;
+    if (!fs.existsSync(path)) {
+      const response2 = await axios.get(`http://vorlesungsplan.dhbw-mannheim.de/ical.php?uid=${uid_list[x]}`);
+      fs.writeFile(path, response2.data, (err) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(`Downloaded ICS Name= ${name_list[x]}; UID= ${uid_list[x]}`);
+        }
+        hash_list.push(`UID= ${uid_list[x]}; Hash:` + crypto.createHash("sha1").update(response2.data).digest("base64"));
+      });
+    } else {
+      console.log(`ICS-File Name= ${name_list[x]}; UID= ${uid_list[x]} exists!`);
+    }
+  }
+  console.log(hash_list);
+  return hash_list;
+}
+
+/** 
+getUIDS().then((value) => {
+  for (let x = 0; x < value[0].length; x++) {
+    console.log(`Name= ${value[0][x]}; UID= ${value[1][x]}`);
+  }
+});
+*/
