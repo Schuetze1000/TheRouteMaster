8d85b353065dba476f18615f95d8c2cb3809c1d4
Leonidas-maker <83175153+Leonidas-maker@users.noreply.github.com>
2023-05-12 19:33:13 +0200

    typo fix

--- client/src/pages/login/login.tsx
+++ client/src/pages/login/login.tsx
@@ -23,7 +23,7 @@ function Login() {
                 <Navbar/>
                 <div className="flex justify-center flex-col m-auto h-screen">
                     <div className="login-box">
-                        <p className="text-center text-black text-3xl">Bitte melde dich an mit deiner Email an</p>
+                        <p className="text-center text-black text-3xl">Bitte melde dich mit deiner Email an</p>
                         <div className="relative mb-3">
                             <input
                                 className="peer m-0 block h-[58px] w-full rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-4 text-base font-normal leading-tight text-neutral-700 transition duration-200 ease-linear placeholder:text-transparent focus:border-primary focus:pb-[0.625rem] focus:pt-[1.625rem] focus:text-neutral-700 focus:outline-none peer-focus:text-primary dark:border-neutral-600 dark:text-neutral-200 dark:focus:border-primary dark:peer-focus:text-primary [&:not(:placeholder-shown)]:pb-[0.625rem] [&:not(:placeholder-shown)]:pt-[1.625rem]"
