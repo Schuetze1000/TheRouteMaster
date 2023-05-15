1d1b8254b2be70d9d5fc42ad2654f9f0650c7112
Leonidas-maker <83175153+Leonidas-maker@users.noreply.github.com>
2023-05-15 08:13:13 +0200

    changed login/registration theme

--- client/src/pages/login/login.tsx
+++ client/src/pages/login/login.tsx
@@ -32,15 +32,19 @@ function Login() {
     };
 
     return (
-        <body className="h-screen bg-gray-600">
+        <body className="h-screen">
+            <div
+                className="relative overflow-hidden bg-cover bg-no-repeat h-full w-full text-center bg-landing m-auto object-none object-center">
+            <div
+                className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed"
+                style={{backgroundColor: "rgba(0, 0, 0, 0.8)"}}>
             <form onSubmit={onSubmit}>
-                <Navbar/>
                 <div className="flex justify-center flex-col m-auto h-screen">
                     <div className="login-box">
                         <p className="text-center text-black text-3xl">Bitte melde dich mit deiner Email an</p>
                         <div className="relative mb-3">
                             <input
-                                className="peer m-0 block h-[58px] w-full rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-4 text-base font-normal leading-tight text-neutral-700 transition duration-200 ease-linear placeholder:text-transparent focus:border-primary focus:pb-[0.625rem] focus:pt-[1.625rem] focus:text-neutral-700 focus:outline-none peer-focus:text-primary dark:border-neutral-600 dark:text-neutral-200 dark:focus:border-primary dark:peer-focus:text-primary [&:not(:placeholder-shown)]:pb-[0.625rem] [&:not(:placeholder-shown)]:pt-[1.625rem]"
+                                className="peer input"
                                 name="email"
                                 id="email"
                                 type="email"
@@ -56,7 +60,7 @@ function Login() {
 
                         <div className="relative mb-3">
                             <input
-                                className="peer m-0 block h-[58px] w-full rounded border border-solid border-neutral-300 bg-transparent bg-clip-padding px-3 py-4 text-base font-normal leading-tight text-neutral-700 transition duration-200 ease-linear placeholder:text-transparent focus:border-primary focus:pb-[0.625rem] focus:pt-[1.625rem] focus:text-neutral-700 focus:outline-none peer-focus:text-primary dark:border-neutral-600 dark:text-neutral-200 dark:focus:border-primary dark:peer-focus:text-primary [&:not(:placeholder-shown)]:pb-[0.625rem] [&:not(:placeholder-shown)]:pt-[1.625rem]"
+                                className="peer input"
                                 name="password"
                                 id="password"
                                 type="password"
@@ -77,7 +81,7 @@ function Login() {
                             Login
                         </button>
                         <p className="text-center text-blacm">Oder melde dich mit deinem Google Account an</p>
-                        <button type="button" className="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2">
+                        <button type="button" className="text-white bg-[#4285F4] rounded-3xl hover:rounded-xl duration-300 hover:bg-[#3271d8] focus:ring-4 focus:outline-none active:bg-[#235dbb] font-medium text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2">
                         <svg className="w-4 h-4 mr-2 -ml-1" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512"><path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path></svg>
                         Mit Google anmelden
                         </button>
@@ -86,6 +90,8 @@ function Login() {
                     </div>
                 </div>
             </form>
+            </div>
+            </div>
         </body>
     );
 }
--- client/src/pages/registration/registration.tsx
+++ client/src/pages/registration/registration.tsx
@@ -32,9 +32,13 @@ function Registration() {
     };
 
     return (
-        <body className="h-screen bg-gray-600">
+        <body className="h-screen">
+            <div
+                className="relative overflow-hidden bg-cover bg-no-repeat h-full w-full text-center bg-landing m-auto object-none object-center">
+            <div
+                className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed"
+                style={{backgroundColor: "rgba(0, 0, 0, 0.8)"}}>
             <form onSubmit={onSubmit}>
-                <Navbar/>
                 <div className="flex justify-center flex-col m-auto h-screen">
                     <div className="login-box">
                         <p className="text-center text-black text-3xl">Registration</p>
@@ -110,7 +114,7 @@ function Registration() {
                             Registrieren
                         </button>
                         <p className="text-center text-blacm">Oder registriere dich mit deinem Google Account</p>
-                        <button type="button" className="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2">
+                        <button type="button" className="text-white bg-[#4285F4] rounded-3xl hover:rounded-xl duration-300 hover:bg-[#3271d8] focus:ring-4 focus:outline-none active:bg-[#235dbb] font-medium text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2">
                         <svg className="w-4 h-4 mr-2 -ml-1" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512"><path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path></svg>
                         Mit Google registrieren
                         </button>
@@ -119,6 +123,8 @@ function Registration() {
                     </div>
                 </div>
             </form>
+            </div>
+            </div>
         </body>
     );
 }
