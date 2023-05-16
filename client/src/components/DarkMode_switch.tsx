8bd54e419fa5609bc8cd7af0a8d0c846507b6872
Leonidas-maker <83175153+Leonidas-maker@users.noreply.github.com>
2023-05-16 19:57:22 +0200

    added text to navbar buttons

--- client/src/components/DarkMode_switch.tsx
+++ client/src/components/DarkMode_switch.tsx
@@ -1,22 +1,25 @@
 import useColorMode from "../hooks/useColorMode";
-import { IoMdContrast } from "react-icons/io"
 
 const DarkMode_switch = () => {
     const [colorMode, setColorMode] = useColorMode();
 
     return (
         <div>
-            <IconDark icon={<IoMdContrast size="28" />} />
+            <DarkMode />
         </div>
     );
 };
 
-const IconDark = ({ icon }) => {
+const DarkMode = ({ text = "Modus wechseln" }) => {
     const [colorMode, setColorMode] = useColorMode();
 
     return (
         <div className="landing-icon group" onClick={() => setColorMode(colorMode === "light" ? "dark" : "light")}>
-            {icon}
+            <svg className="h-8 w-8 text-white dark:text-black hover:text-black dark:hover:text-white"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="12" cy="12" r="1" />  <circle cx="12" cy="12" r="5" />  <circle cx="12" cy="12" r="9" stroke-dasharray=".001 4.03" /></svg>
+
+            <span className="navbar-text group-hover:scale-100">
+                {text}
+            </span>
         </div>
     );
 };
--- client/src/components/Navbar_credentials.tsx
+++ client/src/components/Navbar_credentials.tsx
@@ -13,13 +13,17 @@ const Navbar_credentials = () => {
     );
 };
 
-const NavbarImprint = () => {
+const NavbarImprint = ({ text = "Impressum" }) => {
     const navigate = useNavigate();
-    const handleClickAccount = () => navigate("/imprint");
+    const handleClick = () => navigate("/imprint");
 
     return (
-        <div className="landing-button group" onClick={handleClickAccount}>
-            <svg className="h-8 w-8 text-white"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <rect x="5" y="3" width="14" height="18" rx="2" />  <line x1="9" y1="7" x2="15" y2="7" />  <line x1="9" y1="11" x2="15" y2="11" />  <line x1="9" y1="15" x2="13" y2="15" /></svg>
+        <div className="landing-button group" onClick={handleClick}>
+            <svg className="h-8 w-8 text-white dark:text-black hover:text-black dark:hover:text-white"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <rect x="5" y="3" width="14" height="18" rx="2" />  <line x1="9" y1="7" x2="15" y2="7" />  <line x1="9" y1="11" x2="15" y2="11" />  <line x1="9" y1="15" x2="13" y2="15" /></svg>
+
+            <span className="navbar-text group-hover:scale-100">
+                {text}
+            </span>
         </div>
     );
 };
