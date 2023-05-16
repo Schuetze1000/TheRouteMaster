532d11589317fc589f9cc0ddd1a19e0e4ed7920d
Leonidas-maker <83175153+Leonidas-maker@users.noreply.github.com>
2023-05-16 23:20:35 +0200

    added re captcha

--- client/src/pages/login/login.tsx
+++ client/src/pages/login/login.tsx
@@ -5,6 +5,8 @@ import Back_landing from "../../components/Back_landing";
 import axios from "axios";
 import { Response } from 'express';
 import Navbar_credentials from "../../components/Navbar_credentials";
+import ReCAPTCHA from "react-google-recaptcha"
+import React, { useRef } from 'react';
 
 function Login() {
     const initialState = {
@@ -46,6 +48,8 @@ function Login() {
             </button>
         );
     };
+
+    const captchaRef = useRef(null)
 
     return (
         <body className="h-screen">
@@ -98,6 +102,10 @@ function Login() {
                             className="inline-block rounded-3xl hover:rounded-xl active:bg-green-700 active:text-white transition-all duration-300 cursor-pointer bg-green-500 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-black shadow-[0_4px_9px_-4px_#14a44d] ease-in-out hover:bg-green-600 hover:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.3),0_4px_18px_0_rgba(20,164,77,0.2)] focus:bg-success-600 focus:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.3),0_4px_18px_0_rgba(20,164,77,0.2)] focus:outline-none focus:ring-0 active:bg-success-700 active:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.3),0_4px_18px_0_rgba(20,164,77,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(20,164,77,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.2),0_4px_18px_0_rgba(20,164,77,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.2),0_4px_18px_0_rgba(20,164,77,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(20,164,77,0.2),0_4px_18px_0_rgba(20,164,77,0.1)]">
                             Login
                         </button>
+                        <div className="relative flex items-center justify-center">
+                        <ReCAPTCHA sitekey={process.env.SECRET_KEY}
+                            ref={captchaRef}/>
+                        </div>
                         <p className="text-center text-blacm">Oder melde dich mit deinem Google Account an</p>
                         <button type="button" className="text-white bg-[#4285F4] rounded-3xl hover:rounded-xl duration-300 hover:bg-[#3271d8] focus:ring-4 focus:outline-none active:bg-[#235dbb] font-medium text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2">
                         <svg className="w-4 h-4 mr-2 -ml-1" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512"><path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"></path></svg>
