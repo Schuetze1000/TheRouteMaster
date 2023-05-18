5cbc349ca1a0baaeaac105b45af7c04e0ffd275e
Schuetze1000 <87863203+Schuetze1000@users.noreply.github.com>
2023-05-18 15:24:23 +0200

    Added more Weather Symbols

--- client/src/components/weather.tsx
+++ client/src/components/weather.tsx
@@ -14,68 +14,6 @@ function Weather() {
 	const [isLoading, setLoading] = useState(true);
 	const [isDisabled, setDisable] = useState(false);
 
-	const weatherCodesToStr = {
-		0: "Klarer Himmel",
-		1: "Überwiegend klar",
-		2: "Teilweise bewölkt",
-		3: "Bewölkt",
-		45: "Nebelig",
-		48: "gefrierender Nebel",
-		51: "Leichter Nieselregen",
-		53: "Mäßiger Nieselregen",
-		55: "Starker Nieselregen",
-		56: "Leichter gefrierender Nieselregen",
-		57: "Starker gefrierender Nieselregen",
-		61: "Leichter Regen",
-		63: "Mäßiger Regen",
-		65: "Starker Regen",
-		66: "Leichter gefrierender Regen",
-		67: "Starker gefrierender Regen",
-		71: "Leichter Schneefall",
-		73: "Mäßiger Schneefall",
-		75: "Starker Schneefall",
-		77: "Schneefall",
-		80: "Leichte Regenschauer",
-		81: "Mäßige Regenschauer",
-		82: "Schwere Regenschauer",
-		85: "Leichte Schneeschauer",
-		86: "Schwere Schneeschauer",
-		95: "Gewitter",
-		96: "Gewitter mit leichtem Hagel",
-		99: "Gewitter mit schwerem Hagel",
-	};
-
-	const weatherCodesToIMG = {
-		0: "01",
-		1: "02",
-		2: "03",
-		3: "04",
-		45: "50",
-		48: "50", //? Better Design
-		51: "09",
-		53: "09", //? Better Design
-		55: "10", //? Better Design
-		56: "09", //? Better Design
-		57: "10", //? Better Design
-		61: "09", //? Better Design
-		63: "09", //? Better Design
-		65: "10", //? Better Design
-		66: "09", //? Better Design
-		67: "10", //? Better Design
-		71: "13",
-		73: "13", //? Better Design
-		75: "13", //? Better Design
-		77: "13", //? Better Design
-		80: "09", //? Better Design
-		81: "09", //? Better Design
-		82: "10", //? Better Design
-		85: "13", //? Better Design
-		86: "13", //? Better Design
-		95: "11",
-		96: "11", //? Better Design
-		99: "11", //? Better Design
-	};
-
 	function windDirectionToStr(direction: number) {
 		switch (true) {
 			case 348.75 <= direction || direction < 11.25:
@@ -116,9 +54,71 @@ function Weather() {
 	}
 
 	useEffect(() => {
-		navigator.permissions.query({ name: 'geolocation' }).then((PermissionStatus) => {
+		const weatherCodesToStr = {
+			0: "Klarer Himmel",
+			1: "Überwiegend klar",
+			2: "Teilweise bewölkt",
+			3: "Bewölkt",
+			45: "Nebelig",
+			48: "gefrierender Nebel",
+			51: "Leichter Nieselregen",
+			53: "Mäßiger Nieselregen",
+			55: "Starker Nieselregen",
+			56: "Leichter gefrierender Nieselregen",
+			57: "Starker gefrierender Nieselregen",
+			61: "Leichter Regen",
+			63: "Mäßiger Regen",
+			65: "Starker Regen",
+			66: "Leichter gefrierender Regen",
+			67: "Starker gefrierender Regen",
+			71: "Leichter Schneefall",
+			73: "Mäßiger Schneefall",
+			75: "Starker Schneefall",
+			77: "Schneefall",
+			80: "Leichte Regenschauer",
+			81: "Mäßige Regenschauer",
+			82: "Schwere Regenschauer",
+			85: "Leichte Schneeschauer",
+			86: "Schwere Schneeschauer",
+			95: "Gewitter",
+			96: "Gewitter mit leichtem Hagel",
+			99: "Gewitter mit schwerem Hagel",
+		};
+
+		const weatherCodesToIMG = {
+			0: "01",
+			1: "02",
+			2: "03",
+			3: "04",
+			45: "50",
+			48: "51",
+			51: "06",
+			53: "07", //? Better Design
+			55: "08", //? Better Design
+			56: "09", //? Better Design
+			57: "10", //? Better Design
+			61: "09", //? Better Design
+			63: "09", //? Better Design
+			65: "10", //? Better Design
+			66: "09", //? Better Design
+			67: "10", //? Better Design
+			71: "14",
+			73: "15", 
+			75: "16", 
+			77: "13", //? Better Design
+			80: "09", //? Better Design
+			81: "09", //? Better Design
+			82: "10", //? Better Design
+			85: "13", //? Better Design
+			86: "13", //? Better Design
+			95: "11",
+			96: "11", //? Better Design
+			99: "11", //? Better Design
+		};
+
+		navigator.permissions.query({ name: "geolocation" }).then((PermissionStatus) => {
 			console.log(PermissionStatus.state);
-			if (String(PermissionStatus.state) == "granted") {
+			if (String(PermissionStatus.state) === "granted") {
 				navigator.geolocation.getCurrentPosition((position) => {
 					const optionsWeatherAPI = {
 						method: "GET",
@@ -143,41 +143,40 @@ function Weather() {
 						axios(optionsCity).then((resCity) => {
 							const dataWeatherAPI = resWeatherAPI.data;
 							setCity(resCity.data.city);
-	
+
 							const weathercode = dataWeatherAPI.current_weather.weathercode;
 							var pathWeatherimg = "/weather_icons/";
-	
+
 							if (!weatherCodesToStr[weathercode]) {
 								setWeatherstate("Unknown");
 								pathWeatherimg += "unkown.png";
 								setWeatherimg(pathWeatherimg);
 							} else {
 								setWeatherstate(weatherCodesToStr[weathercode]);
-	
-								if (dataWeatherAPI.current_weather.is_day == 1) {
+
+								if (dataWeatherAPI.current_weather.is_day === 1) {
 									pathWeatherimg += weatherCodesToIMG[weathercode] + "d.png";
 								} else {
 									pathWeatherimg += weatherCodesToIMG[weathercode] + "n.png";
 								}
 								setWeatherimg(pathWeatherimg);
 							}
-	
+
 							setTemperature(dataWeatherAPI.current_weather.temperature);
 							setWindspeed(dataWeatherAPI.current_weather.windspeed);
 							setWindDirection(windDirectionToStr(dataWeatherAPI.current_weather.winddirection));
-	
+
 							const currenttime = dataWeatherAPI.current_weather.time;
 							const timeindex = dataWeatherAPI.hourly.time.indexOf(currenttime);
 							setHumidity(dataWeatherAPI.hourly.relativehumidity_2m[timeindex]);
 							setPressure(dataWeatherAPI.hourly.surface_pressure[timeindex]);
 							setApparentTemperature(dataWeatherAPI.hourly.apparent_temperature[timeindex]);
-	
+
 							setLoading(false);
 						});
 					});
 				});
-			}
-			else {
+			} else {
 				setLoading(false);
 				setDisable(true);
 			}
@@ -189,7 +188,7 @@ function Weather() {
 	}
 
 	if (isDisabled) {
-		return <div/>;
+		return <div />;
 	}
 
 	return (
